f36812a2a55a8b45463e873bdc7a030a45fb386e
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-17 20:56:33 -0300

    04.Datos Primitivos - Edad+Sueldo+Peso+Altura

---
+++ 04.Datos_Primitivos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Datos_Primitivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nEdad; int nSueldo;
+            double nPeso, nAltura;
+
+            nEdad = 35;
+
+            nSueldo = 2500;
+
+            nPeso = 99.00;
+            nAltura = 1.73;
+
+            Console.WriteLine("Edad: " + nEdad);
+            Console.WriteLine("Mi Sueldo: " + nSueldo);
+
+            Console.WriteLine("Mi Peso es de: " + nPeso);
+            Console.WriteLine("Mi Altura es de: " + nAltura);
+            Console.ReadKey(true);
+        }
+    }
+}
