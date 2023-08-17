b22ab3fabb6aa6e671d2c896015d81c3177de5c6
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-17 20:07:00 -0300

    03.Datos_Primitivos - Edad + Sueldo

---
+++ 03.Datos_Primitivos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Datos_Primitivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nEdad; int nSueldo;
+
+            nEdad = 35;
+
+            nSueldo = 2500;
+
+            Console.WriteLine("Edad: " + nEdad);
+            Console.WriteLine("Mi Sueldo: " + nSueldo);
+            Console.ReadKey(true);
+        }
+    }
+}
