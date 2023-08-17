ea7d8a8dfa5280b4af238ff754326020dd6f9c10
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-17 19:43:36 -0300

    Primer Commit - Hola Mundo en C#

---
+++ 01.Hola_Mundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Hola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Bienvenido a C# - Nivel Básico");
+            Console.ReadKey(true);
+        }
+    }
+}
