b3749128f66fb3ebd51d990f13acb8c7e7ab334b
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-17 19:48:22 -0300

    02.Datos_Primitivos - Lectura Variable Edad

---
+++ 02.Datos_Primitivos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Datos_Primitivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nEdad;
+            nEdad = 35;
+            Console.WriteLine(nEdad);
+            Console.ReadKey(true);
+        }
+    }
+}
