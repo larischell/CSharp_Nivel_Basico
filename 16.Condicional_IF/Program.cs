90a3e5cb53a42597e6726f4685c2703a2fdf1177
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 15:10:12 -0300

    16.Condicional_IF - Mayor o Menor de Edad

---
+++ 16.Condicional_IF/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Condicional_IF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.Write("Cual es tu Edad ? ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18){
+                Console.WriteLine("Ya sos Mayor de Edad");
+            }
+            else{
+                Console.WriteLine("Todavía sos Menor de Edad");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
