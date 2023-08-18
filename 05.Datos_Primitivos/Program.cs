d033a67558282bc2e4246ca6b00fb469e2eeee82
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-17 21:00:37 -0300

    05.Datos_Primitivos - + Nombre y Apellido

---
+++ 05.Datos_Primitivos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Datos_Primitivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nEdad = 35;
+            int nSueldo = 2500;
+            double nPeso, nAltura;
+
+            string cNombres, cApellidos;
+
+            cNombres = "Juan Ariel";
+            cApellidos = "Schell";
+
+            nPeso = 99.80;
+            nAltura = 1.73;
+
+            Console.WriteLine("Mi Nombre es: " + cNombres);
+            Console.WriteLine("Mi Apellido es: " + cApellidos);
+
+            Console.WriteLine("Edad: " + nEdad + " años");
+            Console.WriteLine("Mi Sueldo es de: " + nSueldo + " Pesos");
+
+            Console.WriteLine("Mi Peso es de: " + nPeso + " Kg.");
+            Console.WriteLine("Mi Altura es de: " + nAltura + " Mts.");
+
+            Console.ReadKey(true);
+        }
+    }
+}
