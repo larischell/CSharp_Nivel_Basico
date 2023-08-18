f09ff29e2c08b344ab6d40922cc45affac444b3c
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 07:28:14 -0300

    10.Op_Aritmeticos - Res(-),Mul(*),Div(/),Res(%).

---
+++ 10.Op_Aritmeticos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Op_Aritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2;
+
+            valor1 = 10;
+            valor2 = 3;
+
+            Console.WriteLine("La Suma de " + valor1 + " + " + valor2 + " es: " + (valor1 + valor2));
+            Console.WriteLine("La Resta de " + valor1 + " - " + valor2 + " es: " + (valor1 - valor2));
+            Console.WriteLine("La Multiplicación de " + valor1 + " * " + valor2 + " es: " + (valor1 * valor2));
+            Console.WriteLine("La División de " + valor1 + " / " + valor2 + " es: " + (valor1 / valor2));
+            Console.WriteLine("El Residuo de la División " + valor1 + " % " + valor2 + " es: " + (valor1 % valor2));
+            Console.ReadKey(true);
+        }
+    }
+}
