1bc2709291780c3fd69ec528d5041bb9daf03a5e
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 00:23:06 -0300

    07.Variable Dinámica-Tipo dato (bool, string,etc.)

---
+++ 07.Variable_Dinamica/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Variable_Dinamica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nEdad = 35;
+            int nSueldo = 2500;
+            double nPeso, nAltura;
+
+            string cNombres, cApellidos;
+            bool lEstudia;
+
+            dynamic variable_dinamica;
+            variable_dinamica = 2500.67;
+
+            cNombres = "Juan Ariel";
+            cApellidos = "Schell";
+
+            nPeso = 99.80;
+            nAltura = 1.73;
+
+            lEstudia = true;
+
+            Console.WriteLine("Mi Nombre es: " + cNombres);
+            Console.WriteLine("Mi Apellido es: " + cApellidos);
+
+            Console.WriteLine("Edad: " + nEdad + " años");
+            Console.WriteLine("Mi Sueldo es de: " + nSueldo + " Pesos");
+
+            Console.WriteLine("Mi Peso es de: " + nPeso + " Kg.");
+            Console.WriteLine("Mi Altura es de: " + nAltura + " Mts.");
+
+            Console.WriteLine("Estudias? " + lEstudia);
+            
+            // La Variable dinamica puede recibir cualquieer tipo de dato (bool, string,etc).
+            Console.WriteLine("Variable Dinamica " + variable_dinamica);
+
+            Console.ReadKey(true);
+        }
+    }
+}
