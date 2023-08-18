b961994260e1ef30dca838760bcf4199f76b2a95
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 14:21:31 -0300

    14.EntradaYsalida - Nombre+Edad

---
+++ 14.EntradaYsalida/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.EntradaYsalida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            int edad;
+
+            Console.Write("Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Tu Nombre es: " + Nombre);
+            Console.Write("Edad: ");
+            edad=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tu edad "+Nombre+" es "+edad+" años");
+            Console.ReadKey(true);
+        }
+    }
+}
