f26493583fc0ffe77a0323fba2950de66d0abf11
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 13:16:41 -0300

    12.Op_Logicos - AND - OR - NOT

---
+++ 12.Op_Logicos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Op_Logicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            a = 100; b=25; c=45; d=200;
+            Console.WriteLine("Consulta Operador AND a<b y c>d ?: "+((a < b) && (c > d)));
+            Console.WriteLine("Consulta Operador OR a<b y c>d ?: "+((a < b) || (c > d)));
+            Console.WriteLine("Consulta Operador NOT a<b ?: " + (!(a < b)));
+            Console.ReadKey(true);
+        }
+    }
+}
