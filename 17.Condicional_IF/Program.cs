cb2fb0ea8663feb7803fba3a6cf862887a5c85f6
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 15:13:05 -0300

    17.Condicional_IF - Niño, Joven, Adulto / Mayor

---
+++ 17.Condicional_IF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Condicional_IF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.Write("Cual es tu Edad ? ");
+            edad= int.Parse(Console.ReadLine());
+
+            if( edad <= 10 ) {
+                Console.WriteLine("Eres un niño");
+            }else if( edad <= 17 ){
+                Console.WriteLine("Eres un Joven");
+            }else if (edad <= 65){
+                Console.WriteLine("Eres un Adulto");
+            }
+            else{
+                Console.WriteLine("Eres un Adulto Mayor");
+            }
+                Console.ReadKey();
+        }
+    }
+}
