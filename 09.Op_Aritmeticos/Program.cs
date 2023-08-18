1f4df7c9ca1362f9e7c4270d5c4748e4954d70f9
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 07:22:56 -0300

    09.Op_Aritmeticos - Suma (+).

---
+++ 09.Op_Aritmeticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Op_Aritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2;
+
+            valor1 = 5; 
+            valor2 = 15;
+
+            Console.WriteLine("La Suma de " + valor1 + " + " + valor2 + " es: " + (valor1 + valor2));
+            Console.ReadKey(true);
+        }
+    }
+}
