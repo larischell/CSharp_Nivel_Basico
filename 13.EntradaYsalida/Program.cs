ef653ee485020e57d643bbfd8106fc9a9efc1dda
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 14:19:14 -0300

    13.EntradaYsalida - Nombre

---
+++ 13.EntradaYsalida/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.EntradaYsalida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+
+            Console.Write("Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Tu Nombre es: " + Nombre);
+            Console.ReadKey(true);
+        }
+    }
+}
