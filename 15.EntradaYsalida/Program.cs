b4eb7f559159adc85df6a9ff8afa69381b0b27b6
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 14:23:29 -0300

    15.EntradaYsalida - Nombre+Edad+Peso

---
+++ 15.EntradaYsalida/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.EntradaYsalida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            int edad;
+            byte peso;
+
+            Console.Write("Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Tu Nombre es: " + Nombre);
+            Console.Write("Edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tu edad " + Nombre + " es " + edad + " años");
+
+            Console.Write("Peso: ");
+            peso = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Tu Peso " + Nombre + " es " + peso + " kg. ");
+            Console.ReadKey(true);
+        }
+    }
+}
