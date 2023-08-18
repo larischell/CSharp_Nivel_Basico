0722bc20b9b7632866716c13456834529e0f4355
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 00:28:41 -0300

    08.Constantes - DNI

---
+++ 08.Constantes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            const string documento_identidad= "33.126.469";
+            double sueldo;
+
+            nombre = "Juan Ariel";
+            sueldo = 2500;
+
+            Console.WriteLine("Mi Nombre es: " + nombre);
+            Console.WriteLine("Mi Documento de Identidad es: " + documento_identidad);
+            Console.WriteLine("Mi Sueldo es de: " + sueldo);
+
+            Console.ReadKey(true);
+        }
+    }
+}
