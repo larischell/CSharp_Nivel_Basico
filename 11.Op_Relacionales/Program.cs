943113e6df02b47ed6ab7c3368b6c2eaae0a7272
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-18 11:46:55 -0300

    Op_Relacionales - Mayor, Menor, Distinto

---
+++ 11.Op_Relacionales/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Op_Relacionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            bool operacion;
+            a = 15; b=35; c=35;
+            operacion = a < b;
+
+            Console.WriteLine("El Valor de A es Mayor que B? "+(a>b));
+            Console.WriteLine("El Valor de A es Menor que B? " + operacion);
+            Console.WriteLine("El Valor de B no es Igual a C? "+(b!=c));
+
+            Console.ReadKey(true);
+        }
+    }
+}
