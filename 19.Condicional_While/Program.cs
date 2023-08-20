99ad816e2e0b9154d18a28d54ac7401ebb6cc8f8
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-20 20:04:45 -0300

    19.While - Menor o Igual a 10

---
+++ 19.Condicional_While/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Condicional_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            numero = 0;
+            while (numero <= 10) {
+                Console.WriteLine("El numero es igual a: " + numero);
+                numero++;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
