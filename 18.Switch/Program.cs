58d6f63e31b32e209cc18501d16dc27b8c6844d5
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-20 17:06:27 -0300

    18.Switch - Días de la Semana

---
+++ 18.Switch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia;
+
+            Console.Write("Indicar el Dia de la semana en número: ");
+            dia= int.Parse(Console.ReadLine());
+            
+            switch (dia)
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miércoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sábado");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+                default: Console.WriteLine("Dia de semana inexistente");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
