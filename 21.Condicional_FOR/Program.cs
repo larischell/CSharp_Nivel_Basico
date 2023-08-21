3e40338cb111b55e39bad3f9b9d10963d0b23cff
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-21 18:28:59 -0300

    21.Condicional_FOR - Contador <= 20

---
+++ 21.Condicional_FOR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Condicional_FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (int conteo=0; conteo <= 20; conteo++) {
+                Console.WriteLine("EL Valor de la Varieble Conteo es de: "+conteo);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
