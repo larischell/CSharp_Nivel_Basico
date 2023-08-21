3bb15ea2678526140e251bfc817d1177e55aa02e
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-21 16:21:28 -0300

    20.Condicional_DoWhile - Menor a 100 ?

---
+++ 20.Condicional_DoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Condicional_DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            numero = 100;
+
+            do {
+                Console.WriteLine("el valor del número es: "+numero);
+                numero++; 
+            } while (numero < 99);
+
+            Console.ReadKey(true);  
+        }
+    }
+}
