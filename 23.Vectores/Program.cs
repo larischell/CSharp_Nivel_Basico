5c4eaa0d56cb81ac88856d6492ef4079bc652dfb
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-22 16:01:29 -0300

    23.Vectores - N Cantidad de Sueldos

---
+++ 23.Vectores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23.Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidad;
+
+            Console.Write("Indicar N cantidad de Sueldos a Ingresar: ");
+            cantidad=int.Parse(Console.ReadLine());
+
+            double [] sueldos= new double[cantidad];
+            
+            for (int x = 0; x<cantidad; x++) {
+                Console.Write("Ingrese el Sueldo N: " +x+ " : ");
+                sueldos[x]=double.Parse(Console.ReadLine());    
+            }
+            for (int y = 0; y<cantidad; y++)
+            {
+                Console.WriteLine("El Sueldo N "+y+" : " + sueldos[y]);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
