5cf41e193d703d3634116554eaf0fe65554966a3
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-08-22 15:57:26 -0300

    22. Vectores - El Sueldo es de: ...

---
+++ 22.Vectores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sueldo = {900, 1000, 1500, 1800, 2000, 3500 };
+
+            for (int x = 0; x < sueldo.Length; x++)
+            {
+
+                Console.WriteLine("El Sueldo es de: " + sueldo[x]);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
