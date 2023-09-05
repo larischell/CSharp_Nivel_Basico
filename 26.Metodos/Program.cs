903aff41539a1442d66f40e1855171ab774a820d
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-09-05 15:50:25 -0300

    26.Metodos
    Nombres + Apellidos

---
+++ 26.Metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26.Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            datos();
+
+            Console.ReadKey(true);
+        }
+        static void datos() {
+            string nombres, apellidos;
+            Console.Write("Ingrese un Nombre: ");
+            nombres = Console.ReadLine();
+
+            Console.Write("Ingreese un Apellido: ");
+            apellidos = Console.ReadLine();
+
+            Console.WriteLine("Tu Nombre y tu Apellido es: "+nombres+" "+apellidos);
+        }
+    }
+}
