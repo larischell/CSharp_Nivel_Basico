538a54c6eb104f4ed4545424ebf365430d863c56
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-09-05 15:57:36 -0300

    29.Excepciones - Edad + Año de Nacimiento

---
+++ 29.Excepciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29.Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            try
+            {
+                Console.Write("Cual es tu Edad ? ");
+                edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Tu año dee Nacimiento es: " + (2023 - edad));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
