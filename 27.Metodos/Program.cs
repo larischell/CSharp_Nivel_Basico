719e71c1db316d0cee6050aa5da6a6b086654504
Juan Ariel Schell <85419012+larischell@users.noreply.github.com>
2023-09-05 15:53:03 -0300

    27.Metodos - x Nombres + x Apellidos

---
+++ 27.Metodos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27.Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombres, apellidos;
+            Console.Write("Ingrese un Nombre: ");
+            nombres = Console.ReadLine();
+
+            Console.Write("Ingreese un Apellido: ");
+            apellidos = Console.ReadLine();
+
+            datos(nombres, apellidos);
+
+            Console.ReadKey(true);
+        }
+        //Desde este punto vamos a crear metodos 
+        static void datos(string xnombres, string xapellidos)
+        {
+            Console.WriteLine("Tu Nombre y tu Apellido es: " + xnombres + " " + xapellidos);
+        }
+    }
+}
